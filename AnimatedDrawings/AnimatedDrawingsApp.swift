1835a41077efe404329ea0b31825351a9d68ccce
chminipark <chminipark@gmail.com>
2023-05-15 16:53:23 +0900

    first init

---
+++ AnimatedDrawings/AnimatedDrawingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedDrawingsApp.swift
+//  AnimatedDrawings
+//
+//  Created by minii on 2023/05/15.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedDrawingsApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
