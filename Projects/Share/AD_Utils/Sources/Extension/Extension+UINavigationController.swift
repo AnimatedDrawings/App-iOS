1002cd561a7f54a2df6f6f37498a106fcfbe1b8c
chminipark <chminipark@gmail.com>
2023-05-30 20:14:39 +0900

    [Feat] : Swipe Back Gesture 추가
    Git : #6

---
+++ Projects/Share/AD_Utils/Sources/Extension/Extension+UINavigationController.swift
@@ -0,0 +1,20 @@
+//
+//  Extension+UINavigationController.swift
+//  AD_Utils
+//
+//  Created by minii on 2023/05/30.
+//  Copyright © 2023 chminipark. All rights reserved.
+//
+
+import SwiftUI
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+  override open func viewDidLoad() {
+    super.viewDidLoad()
+    interactivePopGestureRecognizer?.delegate = self
+  }
+  
+  public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+    return viewControllers.count > 1
+  }
+}
