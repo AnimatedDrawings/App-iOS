453f9333864a2ac8bb245432f6211faeb330be9f
chminipark <chminipark@gmail.com>
2023-06-30 21:58:39 +0900

    [Refactor] : 파일 위치 변경
    Git : #18

--- Projects/Share/AD_Utils/Sources/Extension/Extension+Moya.swift
+++ Projects/Share/AD_Utils/Sources/Extension/Extension+Moya.swift
@@ -1,15 +1,14 @@
 //
 //  Extension+Moya.swift
-//  AD_Feature
+//  AD_Utils
 //
-//  Created by minii on 2023/06/27.
+//  Created by minii on 2023/06/30.
 //  Copyright © 2023 chminipark. All rights reserved.
 //
 
 import Moya
-import Foundation
 
-extension MoyaProvider {
+public extension MoyaProvider {
   func request(_ target: Target) async -> Result<Response, MoyaError> {
     await withCheckedContinuation { continuation in
       self.request(target) { result in
