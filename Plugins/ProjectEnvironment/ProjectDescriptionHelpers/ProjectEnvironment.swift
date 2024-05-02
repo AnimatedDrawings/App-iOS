15de73e882417a6c97c223933be9469579f4ed8b
chminii <chminipark@gmail.com>
2024-05-02 15:09:39 +0900

    [Refactor] : TCA 버전 업데이트

--- Plugins/ProjectEnvironment/ProjectDescriptionHelpers/ProjectEnvironment.swift
+++ Plugins/ProjectEnvironment/ProjectDescriptionHelpers/ProjectEnvironment.swift
@@ -61,16 +61,25 @@ public extension ProjectDescription.ResourceFileElements {
 
 public extension ProjectDescription.InfoPlist {
   static var AD: Self {
-    return .extendingDefault(with: [
-      "UIMainStoryboardFile": "",
-      "UILaunchStoryboardName": "LaunchScreen",
-      "ENABLE_TESTS": .boolean(true),
-      "NSPhotoLibraryUsageDescription": "We need access to photo library so that photos can be selected",
-      "NSPhotoLibraryAddUsageDescription": "This app requires access to the photo library.",
-      "UIUserInterfaceStyle": "Light",
-      "UISupportedInterfaceOrientations": ["UIInterfaceOrientationPortrait"]
-    ])
+    return .extendingDefault(with: defaultPlistValue)
   }
+  
+  static func extendingAD(with pListValue: [String : Plist.Value]) -> ProjectDescription.InfoPlist {
+    let newPlistValue: [String : Plist.Value] = defaultPlistValue.merging(pListValue) { (_, new) in
+      new
+    }
+    return .extendingDefault(with: newPlistValue)
+  }
+  
+  static let defaultPlistValue: [String : Plist.Value] =  [
+    "UIMainStoryboardFile": "",
+    "UILaunchStoryboardName": "LaunchScreen",
+    "ENABLE_TESTS": .boolean(true),
+    "NSPhotoLibraryUsageDescription": "We need access to photo library so that photos can be selected",
+    "NSPhotoLibraryAddUsageDescription": "This app requires access to the photo library.",
+    "UIUserInterfaceStyle": "Light",
+    "UISupportedInterfaceOrientations": ["UIInterfaceOrientationPortrait"]
+  ]
 }
 
 public extension Project.Options {
