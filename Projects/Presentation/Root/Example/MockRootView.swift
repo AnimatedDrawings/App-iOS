0f6c0ad8b809524e0eb0c03eba0f6ecfeec4e7d6
chminii <chminipark@gmail.com>
2025-04-01 14:20:06 +0900

    [Test] : RootViewExample example1 state 추가
    Git : #104

--- Projects/Presentation/Root/Example/MockRootView.swift
+++ Projects/Presentation/Root/Example/MockRootView.swift
@@ -11,12 +11,47 @@ import NetworkProvider
 import RootFeatures
 import SharedProvider
 import SwiftUI
+import MakeADFeatures
+import ADUIKit
+import DomainModels
+import ADResources
+
+
+extension MakeADFeature.State {
+  public static func example1() -> Self {
+    let example1 = ADResourcesAsset.Example1TestImages.self
+    
+    return .init(
+      step: .init(
+        isShowStepBar: true,
+        currentStep: .FindCharacterJoints,
+        completeStep: .FindCharacterJoints
+      ),
+      makeADInfo: .init(
+        originalImage: example1.originImage.image,
+        boundingBox: BoundingBox.example1().cgRect,
+        initMaskImage: example1.mask.image,
+        croppedImage: example1.texture.image,
+        maskedImage: example1.cutoutCharacterImage.image,
+        joints: Joints.example1()
+      ),
+      uploadDrawing: .init(),
+      findTheCharacter: .init(),
+      separateCharacter: .init(),
+      findCharacterJoints: .init()
+    )
+  }
+}
 
 struct MockRootView: View {
   @Bindable var store: StoreOf<RootFeature>
 
   init() {
-    let state = RootFeature.State()
+    let state = RootFeature.State(
+      adViewState: .ConfigureAnimation,
+      makeAD: .example1(),
+      configureAnimation: .init()
+    )
     store = Store(initialState: state) {
       RootFeature()
         .dependency(
